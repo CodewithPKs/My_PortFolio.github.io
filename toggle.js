ea01fa0e608a82be46878ad0ef2a878dbe3f2bc6
praveen soni <ece.praveen2003@gmail.com>
2023-09-25 03:33:36 +0530

    fifthenth commit

--- toggle.js
+++ toggle.js
@@ -4,3 +4,10 @@ const openProject = document.getElementById('onclick-show');
 showProject.addEventListener('click', () => {
     openProject.classList.toggle('wrapper');
 })
+
+const showFlutterProject = document.getElementById('project-show3');
+const openFlutterProject = document.getElementById('onclick-show3');
+
+showFlutterProject.addEventListener('click', () => {
+    openFlutterProject.classList.toggle('wrapper');
+})
